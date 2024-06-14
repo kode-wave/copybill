c03df90a93a00622b9a84d177eb3c57194081a7f
mohdaidi <aidi@kodewave.com>
2024-06-15 01:01:33 +0800

    updated controller

---
+++ src/Controllers/BillCopyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BillCopy\BillCopy\BillCopyController;
+
+use App\Http\Requests\Request;
+
+class BillCopyController
+{
+
+    public function getCopyBill(Request $request)
+    {
+        return 'hello';
+    }
+
+}
