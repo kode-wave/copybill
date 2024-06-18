43805edc2bd4e350af960de4dbd72562e873d1dd
mohdaidi <aidi@kodewave.com>
2024-06-18 22:46:27 +0800

    updated controller

--- config/bill_copy.php
+++ config/bill_copy.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
+    'auth_token' => 'e68d0073eedef0b84eed424bf51d0b23a3f90231',
     'model' => 'App\Models\EL\ApplicationJournal',
+    'primary_column' => 'id',
     'column'   => 'invoice_pdf_path',
     'use_function_for_getter' => true,
-    'function_name' => 'getGeneratedBillPdfPath',
-    'function_is_static' => true,
+    'function_name' => 'getGeneratedBillPdfPathUrl',
+    'function_is_static' => false,
     'asset_prefix_directory'    => env('ASSET_STORAGE_PATH', 'assets/uploads').'/',
 ];
--- src/Controllers/BillCopyController.php
+++ src/Controllers/BillCopyController.php
@@ -2,14 +2,42 @@
 
 namespace BillCopy\BillCopy\Controllers;
 
-use App\Http\Requests\Request;
+use Illuminate\Http\Request;
 
 class BillCopyController
 {
 
-    public function getCopyBill(Request $request)
+    public function getCopyBill(Request $request, $id)
     {
-        return 'hello';
+    	$token = config('bill_copy.auth_token');
+        if(isset($request['token']) and $request['token'] == $token){
+            $model = config('bill_copy.model');
+            $primaryKey = config('bill_copy.primary_column');
+            $obj = $model::where($primaryKey, $id)->first();
+            if($obj){
+                $useFunction = config('bill_copy.use_function_for_getter');
+                if($useFunction){
+                    $useFunction = config('bill_copy.function_name');
+                    $functionIsStatic = config('bill_copy.function_is_static');
+                    if($functionIsStatic){
+                        $path = $model::$useFunction($id);
+                    }else{
+                        $path = $obj->$useFunction();
+                    }
+                }
+                if(!$path){
+                    return 'not found';
+                }
+                $results = [
+                    'result'    => $path
+                ];
+                $statusCode=200;
+                $response = \Response::make($results, $statusCode);
+                $response->header('Content-Type', 'application/json');
+                return $response;
+            }
+        }
+        return 'token not found';
     }
 
 }
